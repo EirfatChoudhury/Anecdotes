cab0cc00a9e1db222c2e381aa2c234b04b3d9e88
Eirfat Choudhury <eirfathchoudhury@gmail.com>
2023-02-28 11:16:43 +0000

    Starting point of anecdote app which uses routed anecdotes with the help of react-router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { NotifContextProvider } from './NotifContext'
-
 import App from './App'
 
-const queryClient = new QueryClient()
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <NotifContextProvider>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </NotifContextProvider>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
